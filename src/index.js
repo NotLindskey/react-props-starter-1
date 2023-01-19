f70822ec4a00d1e38845cfc2927cbbda24bc0b0d
Linssi Colin Ruiz <linssi101@gmail.com>
2023-01-18 20:21:58 -0600

    created a another reducer called elementLIst.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App.jsx";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// reducer
+const count = (state = 0, action) => {
+	console.log("inside reducer");
+	return state;
+};
+
+// another reducer
+const elementList = (state = [], action) => {
+	return state;
+};
+
+// store
+const storeInstance = createStore(count);
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={storeInstance}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
